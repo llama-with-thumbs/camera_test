87c4a4e5a1944d036ff668c417c688a4cafd01be
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-10-06 21:21:43 -0400

    feat: only cv2

--- camera-test.py
+++ camera-test.py
@@ -1,20 +1,28 @@
-from PIL import Image
-import subprocess
-import os
+import cv2
 
-def capture_and_save_image(output_path):
+def capture_image_and_save(output_path):
     try:
-        # Specify the full path to the raspistill command
-        raspistill_cmd = "/usr/bin/raspistill"
+        # Initialize the camera (0 represents the default camera)
+        cap = cv2.VideoCapture(0)
 
-        # Use the raspistill command to capture an image
-        subprocess.run([raspistill_cmd, "-o", output_path])
+        # Check if the camera is opened successfully
+        if not cap.isOpened():
+            print("Error: Could not open the camera.")
+            return
 
-        # Open the captured image with Pillow
-        image = Image.open(output_path)
+        # Capture a single frame (an image)
+        ret, frame = cap.read()
 
-        # Save the image (you can also perform further processing here)
-        image.save(output_path)
+        # Check if the frame was captured successfully
+        if not ret:
+            print("Error: Could not capture the frame.")
+            return
+
+        # Save the captured image
+        cv2.imwrite(output_path, frame)
+
+        # Release the camera
+        cap.release()
 
         print(f"Image captured and saved to {output_path}")
     except Exception as e:
@@ -24,5 +32,5 @@ if __name__ == "__main__":
     # Specify the path where you want to save the image
     image_output_path = "image.jpg"
 
-    # Call the capture_and_save_image function to capture and save the image
-    capture_and_save_image(image_output_path)
+    # Call the capture_image_and_save function to capture and save the image
+    capture_image_and_save(image_output_path)
