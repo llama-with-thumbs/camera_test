85faaece71fe4d9a34e69679e55276aafa47c085
Vivi Filitovich <vlad.filitovich@gmail.com>
2023-10-06 21:09:23 -0400

    fix: replase image.jpg

--- camera-test.py
+++ camera-test.py
@@ -1,23 +1,25 @@
 from PIL import Image
+import subprocess
+import os
 
-def save_image_with_pillow(input_path, output_path):
+def capture_and_save_image(output_path):
     try:
+        # Use the raspistill command to capture an image
+        subprocess.run(["raspistill", "-o", output_path])
+
         # Open the captured image with Pillow
-        image = Image.open(input_path)
+        image = Image.open(output_path)
 
         # Save the image (you can also perform further processing here)
         image.save(output_path)
 
-        print(f"Image saved to {output_path}")
+        print(f"Image captured and saved to {output_path}")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
-    # Specify the path of the captured image
-    captured_image_path = "image.jpg"
+    # Specify the path where you want to save the image
+    image_output_path = "image.jpg"
 
-    # Specify the path where you want to save the processed image
-    processed_image_path = "processed_image.jpg"
-
-    # Call the save_image_with_pillow function to open and save the image
-    save_image_with_pillow(captured_image_path, processed_image_path)
+    # Call the capture_and_save_image function to capture and save the image
+    capture_and_save_image(image_output_path)
